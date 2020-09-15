998e8378faec7c8bd94a6f8c29656c164ad2bae2
Eugenia Nunez <eugeiq@gmail.com>
2020-09-14 22:21:26 -0300

    Agregue funcionalidad a los filtros y estile imagenes de productos con transiciones y transformaciones

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const filtroBusqueda = document.getElementById("busqueda");
 const tarjetas = document.getElementsByClassName("tarjeta-producto");
-
+const filtroPuntaje = document.getElementsByClassName("review-filtro");
+const checkboxes = document.querySelectorAll(".review-filtro");
+const botonLimpiar = document.querySelector(".boton-limpiar");
 
 filtroBusqueda.oninput = () => { 
   for (let tarjeta of tarjetas) {
@@ -15,6 +17,51 @@ filtroBusqueda.oninput = () => {
     }
   }
 };
+
+for(let checkbox of filtroPuntaje) {
+  checkbox.onclick = () => {
+    filtrarTarjetas();
+  }
+};
+
+const hayCheckboxSeleccionado = () => {
+  for (let checkbox of filtroPuntaje) {
+    if (checkbox.checked) {
+      return true;
+    }
+  }
+};
+
+const coincidenCheckboxYTarjeta = tarjeta => {
+  const puntaje = tarjeta.dataset.rating;
+  for (let checkbox of filtroPuntaje) {
+    if (checkbox.value === puntaje && checkbox.checked) {
+      return true;
+    }
+  }
+};
+
+const filtrarTarjetas = () => {
+  for (let tarjeta of tarjetas) {
+    tarjeta.classList.add('hidden');
+    if (hayCheckboxSeleccionado()) {
+      if (coincidenCheckboxYTarjeta(tarjeta)) {
+        tarjeta.classList.remove('hidden');
+      }
+    }
+    else {
+      tarjeta.classList.remove('hidden')
+    }
+  }
+};
+
+botonLimpiar.onclick = () => {
+  filtroBusqueda.value = ""
+  for (let checkbox of checkboxes) {
+    checkbox.checked = false 
+  }
+  tarjeta.classList.remove('hidden')
+}
 
 
 
